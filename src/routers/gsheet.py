9065f54c90c1fea2cd7cf7250f943c4b6253723f
vasgg <vas.g@icloud.com>
2025-07-29 15:46:38 +0400

    working on gsheet

--- src/routers/gsheet.py
+++ src/routers/gsheet.py
@@ -3,24 +3,26 @@ import logging
 
 from oauth2client.service_account import ServiceAccountCredentials
 import gspread
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
 from starlette import status
+from src.routers.deps import get_current_username, security
 
 router = APIRouter()
 logger = logging.getLogger(__name__)
 
 
 @router.post("/update/{cell}/{value}")
-async def update_google_sheet(cell: str, value: int):
+async def update_google_sheet(cell: str, value: int, credentials=Depends(security)):
+    get_current_username(credentials)
     scope = [
         "https://spreadsheets.google.com/feeds",
         "https://www.googleapis.com/auth/drive",
     ]
     try:
-        credentials = ServiceAccountCredentials.from_json_keyfile_name(
+        credentials_gs = ServiceAccountCredentials.from_json_keyfile_name(
             "credentials.json", scope
         )
-        client = gspread.authorize(credentials)
+        client = gspread.authorize(credentials_gs)
         spreadsheet = client.open("users_counter")
         worksheet = spreadsheet.sheet1
         await asyncio.to_thread(worksheet.update, cell, [[value]])
